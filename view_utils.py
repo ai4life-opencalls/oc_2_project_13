40d82892557e39b20bba78b99890276de1cecef6
Edoardo Giacomello <edoardo.giacomello@fht.org>
2025-01-30 10:04:05 +0100

    [Fix] GIF export and added looping

--- view_utils.py
+++ view_utils.py
@@ -11,7 +11,7 @@ def create_gif_from_numpy(array, output_file, scale_factor=1.0, frame_skip=10, f
     Create a GIF from a numpy array using matplotlib.
     
     Parameters:
-    - array: numpy array of shape [1000, 1024, 1024]
+    - array: numpy array of shape [time, height, width]
     - output_file: str, the filename of the output GIF
     - scale_factor: float, the factor by which to scale the frames
     - frame_skip: int, the number of frames to skip
@@ -21,37 +21,28 @@ def create_gif_from_numpy(array, output_file, scale_factor=1.0, frame_skip=10, f
     - None (Saves a GIF file)
     """
     frames = []
+    vmin, vmax = array.min(), array.max()
 
-    # Loop through the array with frame skipping
     for i in range(0, array.shape[0], frame_skip):
         frame = array[i]
 
-        # Rescale the frame using nearest-neighbor interpolation
         if scale_factor != 1.0:
-            frame = zoom(frame, zoom=(scale_factor, scale_factor), order=0)  # nearest-neighbor interpolation
+            frame = zoom(frame, zoom=scale_factor, order=0)  # Nearest-neighbor interpolation
 
-        # Plot the frame using matplotlib
         fig, ax = plt.subplots(figsize=(frame.shape[1] / 100, frame.shape[0] / 100), dpi=100)
-        ax.imshow(frame, vmin=array.min(), vmax=array.max(), cmap='gray')
-        ax.axis('off')  # Turn off the axis
+        ax.imshow(frame, vmin=vmin, vmax=vmax, cmap='gray')
+        ax.axis('off')
 
-        # Adjust the figure layout to remove margins
         plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
-        # Save the plot to a buffer instead of showing it
         fig.canvas.draw()
-        image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+        image = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
         image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
         frames.append(image)
 
-        plt.close(fig)
+        plt.close(fig)  # Free memory
     
-    # Create GIF using imageio
-    imageio.mimsave(output_file, frames, fps=fps)
-    
-    # Create GIF using imageio
-    imageio.mimsave(output_file, frames, fps=fps)
-
+    imageio.mimsave(output_file, frames, fps=fps, loop=0)
 
 
 def display_image_with_slider(images, vlim: Literal["stack", "frame", "all"]="stack", titles: np.ndarray = None, **fig_kw):
